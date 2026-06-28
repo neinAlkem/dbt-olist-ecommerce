879c685546388df8374443886e7e6ae2f9e7fa2c
Baghas Rizaluddin <bagasrizal.work@gmail.com>
2026-06-28 23:33:15 +0700

    dbt(models): add staging layers

---
+++ pipelines/include/dbt/olist_analytics/models/staging/staging_orders.sql
@@ -0,0 +1,12 @@
+SELECT
+    TRIM(order_id) AS order_id,
+    TRIM(customer_id) AS customer_id,
+    TRIM(order_status) AS order_status,
+    TRIM(CAST(order_purchase_timestamp AS TIMESTAMP)) AS order_purchase_timestamp,
+    TRIM(CAST(order_approved_at AS TIMESTAMP)) AS order_approved_at,
+    TRIM(CAST(order_delivered_carrier_date AS TIMESTAMP)) AS order_delivered_carrier_date,
+    TRIM(CAST(order_delivered_customer_date AS TIMESTAMP)) AS order_delivered_customer_date,
+    TRIM(CAST(order_estimated_delivery_date AS TIMESTAMP)) AS order_estimated_delivery_date,
+    CURRENT_TIMESTAMP() AS load_timestamp
+FROM
+    {{ source('raw', 'olist_orders_dataset') }}
