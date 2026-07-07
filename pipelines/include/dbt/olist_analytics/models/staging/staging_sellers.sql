37851cf5d6775fd070da2a5618d968abceceff1a
Baghas Rizaluddin <bagasrizal.work@gmail.com>
2026-07-07 12:38:59 +0700

    dbt(models): add scd_Id for type 2 transformation

--- pipelines/include/dbt/olist_analytics/models/staging/staging_sellers.sql
+++ pipelines/include/dbt/olist_analytics/models/staging/staging_sellers.sql
@@ -1,8 +1,14 @@
 SELECT 
-    TRIM(seller_id) AS seller_id,
+    TRIM(seller_id) AS seller_key,
     TRIM(CAST(seller_zip_code_prefix AS CHAR(5))) AS seller_zip_code_prefix,
     TRIM(seller_city) AS seller_city,
     TRIM(seller_state) AS seller_state,
+    MD5(CONCAT(
+            COALESCE(CAST(seller_id AS STRING),''),
+            COALESCE(CAST(seller_zip_code_prefix AS STRING),''),
+            COALESCE(seller_city,''),
+            COALESCE(seller_state,'')
+        )) AS scd_id,
     CURRENT_TIMESTAMP() AS load_timestamp
 FROM
     {{ source('raw', 'olist_sellers_dataset') }}
