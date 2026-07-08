68336aaeaf631ebe19b471bcb546b61cb873b5f4
Baghas Rizaluddin <bagasrizal.work@gmail.com>
2026-07-08 15:39:41 +0700

    dbt(tests): update test documentation

--- pipelines/include/dbt/olist_analytics/tests/date_orders.sql
+++ pipelines/include/dbt/olist_analytics/tests/date_orders.sql
@@ -1,3 +1,12 @@
+/* ---------------------------------- TEST ---------------------------------- */
+
+-- Test to check if all order_approved_at, order_delivered_carrier_date, and order_delivered_customer_date values in staging_orders are greater than or equal to order_purchase_timestamp
+-- Target table: staging_orders
+-- Test description: This test ensures that all order_approved_at, order_delivered_carrier_date, and order_delivered_customer_date values in staging_orders are greater than or equal to order_purchase_timestamp. If any values do not meet this criteria, they will be flagged as failing the test.
+-- Last Updated: 08/07/2026
+
+/* ----------------------------------- END ---------------------------------- */
+
 SELECT 
     *
 FROM
