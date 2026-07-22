4c77cb6148f69538ca24ba2a995bc094d1107e77
Baghas Rizaluddin <bagasrizal.work@gmail.com>
2026-07-22 19:40:15 +0700

    dbt(models): add incremental_hash and load_timestamp fields to properties.yml and staging_order_reviews model

--- pipelines/include/dbt/olist_analytics/models/staging/staging_order_reviews.sql
+++ pipelines/include/dbt/olist_analytics/models/staging/staging_order_reviews.sql
@@ -4,6 +4,7 @@ SELECT
     TRIM(CAST(review_score AS INT)) AS review_score,
     TRIM(review_comment_title) AS review_comment_title,
     TRIM(review_comment_message) AS review_comment_message,
+    MD5(COALESCE(CONCAT(review_id,order_id),'')) AS incremental_hash,
     CURRENT_TIMESTAMP() AS load_timestamp
 FROM
     {{ source('raw', 'olist_order_reviews_dataset') }}
